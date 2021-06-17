c074bd1ac74a9de82bacafb41b63aff92dc65c79
ThangDao01 <thangddth2009001@fpt.edu.vn>
2021-06-18 00:52:54 +0700

    first commit

---
+++ Lab6_2/Thermostat.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab6_2
+{
+    public class Thermostat
+    {
+        public delegate void TemperatureChangeHandler(
+            float newTemperature);
+
+// Define the event publisher
+        private TemperatureChangeHandler _onTemperatureChange;
+        public TemperatureChangeHandler OnTemperatureChange
+        {
+            get { return _onTemperatureChange; }
+            set { _onTemperatureChange = value; }
+        }
+        public float CurrentTemperature
+        {
+            get { return _CurrentTemperature; }
+            set
+            {
+                if (value != CurrentTemperature)
+                {
+                    _CurrentTemperature = value;
+// Call subscribers
+                    if (OnTemperatureChange!=null)
+                    {
+                        OnTemperatureChange(value);
+                    }
+                }
+            }
+        }
+        private float _CurrentTemperature;
+    }
+}
